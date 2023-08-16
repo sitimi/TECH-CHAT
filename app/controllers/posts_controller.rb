a4c75227df35c78dac74e4b0a1abec7156080087
sitimi <sitimi522@gmail.com>
2023-08-16 12:04:00 +0900

    posts create定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,13 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    Post.create(content_params)
+    redirect_to action: :index
+  end
+
+  private
+    def content_params
+      params.require(:post).permit(:title, :content, :name)
+    end
 end
